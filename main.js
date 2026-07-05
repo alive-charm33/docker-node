0107d25baa8a55927fc615fe5da5c1ab7b340d15
bargalesejal@gmail.com <bargalesejal@gmail.com>
2026-07-05 19:07:00 +0530

    Build Node.js Docker application with Express and Dockerfile

---
+++ main.js
@@ -0,0 +1,10 @@
+const express=require('express');
+const app=express();
+const port=process.env.PORT || 8000;
+
+app.get('/',(req,res)=>{
+    return res.json({message: 'Hello I am node.js in container'});
+});
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+});
